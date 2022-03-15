aba148c1f5df74d86d369f6c1d290a5ddfe82cd3
alexthegreat194 <alexander.gentry@aol.com>
2022-03-15 15:25:50 -0700

    working on signup

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,24 @@
+from datetime import datetime
 from typing import Optional
 from fastapi import APIRouter
 from pydantic import BaseModel
+from passlib.context import CryptContext
 
+from app.models import User
+from app.database import engine
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
 
 router = APIRouter(tags=['auth'])
+
+# auth helpers
+
+def verify_passoword(password, hashed_password):
+    return pwd_context.verify(password, hashed_password)
+
+def generate_password_hash(password):
+    return pwd_context.hash(password)
 
 # Schema
 class UserInput(BaseModel):
@@ -15,6 +30,15 @@ class ReturnStatus(BaseModel):
     msg: Optional[str] = None
 
 @router.post('/signup', response_model=ReturnStatus)
-def signup(input: UserInput):
-    print(input)
+async def signup(input: UserInput):
+    print(input.json())
+
+    pw_hash = generate_password_hash(input.password)
+    new_user = User(
+        username=input.username,
+        password_hash=pw_hash,
+        created_at=datetime.now()
+    )
+    print(new_user)
+
     return ReturnStatus()
