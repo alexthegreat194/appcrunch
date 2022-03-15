685384e6675192380f6c64dd46c57691560fe1c8
alexthegreat194 <alexander.gentry@aol.com>
2022-03-15 11:47:57 -0700

    user model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import Field
 from sqlmodel import SQLModel
 
 # User
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    password_hash: str 
+    created_at: datetime
+
+    email: Optional[str]
+    bio: Optional[str]
+    pronouns: Optional[str]
+    website: Optional[str]
+
+
 # Idea
+
+
 # Comment
 # Upvote
