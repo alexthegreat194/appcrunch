1969dcf838f13378a4c4cd836759227e3a66db64
alexthegreat194 <alexander.gentry@aol.com>
2022-03-15 12:04:43 -0700

    comment model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import Field
+from sqlalchemy import table
 from sqlmodel import SQLModel
 
 # User
@@ -21,13 +22,24 @@ class User(SQLModel, table=True):
 # Idea
 class Idea(SQLModel, table=True):
     __tablename__ = 'idea'
-    
+
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     description: str 
-    created_at: str 
+    created_at: datetime 
 
     user_id: int = Field(default=None, foreign_key='user.id')
 
 # Comment
+class Comment(SQLModel, table=True):
+    __tablename__ = 'comment'
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    content: str 
+    created_at: datetime
+
+    user_id: int = Field(default=None, foriegn_key='user.id')
+    idea_id: int = Field(default=None, foriegn_key='idea.id')
+    reply_id: int = Field(default=None, foriegn_key='comment.id')
+
 # Upvote
