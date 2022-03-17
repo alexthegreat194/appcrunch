e949ec73b29e752cc49b66155b1b2c2327c363c5
alexthegreat194 <alexander.gentry@aol.com>
2022-03-17 15:53:47 -0700

    added router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.routers import auth, users, ideas
+from app.routers import auth, users, ideas, comment
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(ideas.router)
+app.include_router(comment.router)
 
 @app.get('/')
 def index():
