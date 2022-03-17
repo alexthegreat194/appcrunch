9af957015983287b148e556c7017d5f2bae8b43f
alexthegreat194 <alexander.gentry@aol.com>
2022-03-16 19:36:02 -0700

    new idea

--- app/routers/ideas.py
+++ app/routers/ideas.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from datetime import datetime
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from sqlmodel import Session, select
 
 from app.models import Idea, User
 from app.database import engine
+from app.schema import ReturnStatus
+from app.routers.auth import get_current_user
 
 router = APIRouter(tags=['ideas'])
 
@@ -15,3 +19,20 @@ def get_all_ideas():
         result = session.exec(statement).all()
         ideas = result
     return ideas 
+
+class IdeaNew(BaseModel):
+    title: str
+    description: str
+
+@router.post('/ideas', response_model=ReturnStatus, )
+def create_idea(data: IdeaNew, user: User = Depends(get_current_user)):
+    with Session(engine) as session:
+        idea = Idea(
+            title=data.title,
+            description=data.description,
+            created_at=datetime.now(),
+            user_id=user.id
+        )
+        session.add(idea)
+        session.commit()
+    return ReturnStatus()
