6a375a14f8af4a377d8803d24681ed215cb6d254
alexthegreat194 <alexander.gentry@aol.com>
2022-03-17 15:46:13 -0700

    ideas get_id and delete

--- app/routers/ideas.py
+++ app/routers/ideas.py
@@ -1,8 +1,10 @@
+from ast import Return
 from datetime import datetime
 from pprint import pprint
 from typing import List
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+import sqlalchemy
 from sqlmodel import Session, select
 from sqlalchemy.orm import selectinload
 
@@ -19,6 +21,15 @@ def get_all_ideas():
         statement = select(Idea)
         results = session.exec(statement).all()
         return results
+
+@router.get('/ideas/{id}', response_model=IdeaReadWithRel)
+def get_idea_by_id(id: int):
+    with Session(engine) as session:
+        print('Statement')
+        statement = sqlalchemy.select(Idea).where(Idea.id == id).options(selectinload('*'))
+        print('Idea')
+        idea = session.execute(statement).scalars().first()
+        return idea
 
 class IdeaNew(BaseModel):
     title: str
@@ -52,4 +63,18 @@ def edit_idea(idea_id: int, data: IdeaBase, user: User = Depends(get_current_use
         session.commit()
         session.refresh(idea)
     
+    return ReturnStatus()
+
+@router.delete('/ideas/{idea_id}', response_model=ReturnStatus)
+def delete_idea(idea_id: int, user: User = Depends(get_current_user)):
+    with Session(engine) as session:
+        idea = session.get(Idea, idea_id)
+        if not idea:
+            return ReturnStatus(success=False, msg='idea does not exits')
+        if idea.user_id is not user.id:
+            return ReturnStatus(success=False, msg='user does not own idea')
+
+        session.delete(idea)
+        session.commit()
+
     return ReturnStatus()
